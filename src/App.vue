fe17870380b868982f99b691c8a76a66592d4495
TobiasVPini <tobias.villarroel@solarisdevelopments.com>
2024-02-05 15:56:07 +0100

    change component styles and comment one signal use

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
+<template>
+  <div class="h-screen relative">
+    <div v-if="isVisible" :class="isVisible ? 'bg-black h-screen w-full opacity-40 absolute z-0': ''"></div>
+    <AddToHomeScreen 
+      v-if="isVisible" 
+      :class="isVisible ? `
+        w-64 
+        border-2 
+        border-orange-100 
+        text-sm 
+        font-extralight 
+        bg-orange-200 
+        rounded-r-full 
+        drop-shadow-xl
+        transition-shadow
+        p-4
+        text-center
+        container
+        z-50
+        absolute 
+        top-1/2 
+        right-0
+        mt-32
+        -translate-x-1/2 
+        -translate-y-1/2
+      ` : ''" 
+      :isVisible="isVisible"
+      v-model:isVisible="isVisible"
+    />
+    <HomeView />
+  </div>
+</template>
+
 <script>
 import HomeView from './views/HomeView.vue';
 import AddToHomeScreen from './components/AddToHomeScreen.vue';
@@ -13,33 +46,25 @@ export default {
         HomeView,
         AddToHomeScreen
     },
-    created() {
-      if (Notification.permission === 'granted') {
-        this.$OneSignal.User.PushSubscription.optIn();
-      } else if (Notification.permission !== 'denied') {
-        this.$OneSignal.Notifications.requestPermission().then(permission => {
-          if (permission) {
-            this.$OneSignal.User.PushSubscription.optIn();
-          }
-        });
-      }
-    }
+    // created() {
+    //   if (Notification.permission === 'granted') {
+    //     this.$OneSignal.User.PushSubscription.optIn();
+    //   } else if (Notification.permission !== 'denied') {
+    //     this.$OneSignal.Notifications.requestPermission().then(permission => {
+    //       if (permission) {
+    //         this.$OneSignal.User.PushSubscription.optIn();
+    //       }
+    //     });
+    //   }
+    // }
 }
 </script>
-
-<template>
-  <div class="h-screen">
-    <AddToHomeScreen v-if="isVisible" :class="isVisible ? 'w-full border-2 border-x-0 border-black text-center p-2 leading-7 absolute bottom-0 z-50 bg-white' : ''" :isVisible="isVisible"/>
-    <HomeView />
-  </div>
-</template>
 
 <style>
 body,
 html {
   padding: 0;
   margin: 0;
-  /* overflow: hidden; */
 }
 * {
   box-sizing: border-box;
