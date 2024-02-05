ee43f7f8f570d2a484494346431e30e3758ea5f9
TobiasVPini <tobias.villarroel@solarisdevelopments.com>
2024-02-05 10:23:37 +0100

    config and index.html  updated

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mi-turno.github.io/',
   plugins: [
     vue(),
     tailwindcss(),
